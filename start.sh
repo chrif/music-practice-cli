86660efeca41ba09e00f7e05e395ee45564b022f
Christian Fecteau <christian.fecteau@gmail.com>
2017-05-06 09:09:36 -0400

    apps to call api

--- start.sh
+++
@@ -1,6 +0,0 @@
-pushd $(dirname "${0}") > /dev/null
-basedir=$(pwd -L)
-# Use "pwd -P" for the path without links. man bash for more info.
-popd > /dev/null
-
-php -d date.timezone="America/Montreal" ${basedir}/api.php start "BWV 639"
